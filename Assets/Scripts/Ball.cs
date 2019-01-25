db7f2e917aa0ee930b22950cc5a9f6b3f1a5d727
Eric <eabigm@gmail.com>
2019-01-25 13:56:20 -0500

    Click a ball to make it move

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody))]
+public class Ball : MonoBehaviour
+{
+    Rigidbody rBody;
+
+    [SerializeField]
+    float maxForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        rBody.AddForce(Random.Range(0, maxForce), Random.Range(0, maxForce), Random.Range(0, maxForce), ForceMode.Impulse);
+    }
+}
