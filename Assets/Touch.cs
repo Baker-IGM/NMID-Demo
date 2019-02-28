b9b91422aab4cba0f262cb5d3a650d728f9485e9
Eric Baker <8997206+patches34@users.noreply.github.com>
2019-02-28 11:29:30 -0500

    Drag (#1)
    * Anchored the ball to the ceiling
    
    * Made web build
    
    * Added chain texture

---
+++ Assets/Touch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch : MonoBehaviour
+{
+    [SerializeField]
+    float width, height;
+
+    [SerializeField]
+    Vector3 pointerWorldPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            pointerWorldPos.x = (pointerWorldPos.x < 0) ? 0 : Input.mousePosition.x / Screen.width;
+            pointerWorldPos.y = (pointerWorldPos.y < 0) ? 0 : Input.mousePosition.y / Screen.height;
+        }
+    }
+}
